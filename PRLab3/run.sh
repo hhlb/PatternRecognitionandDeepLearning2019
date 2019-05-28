33437f480d8b96878b5fdcd365fdab5127223255
hhlb <hhlb1997@outlook.com>
2019-05-28 11:59:20 +0800

    添加了更全面的测试结果

--- PRLab3/run.sh
+++ PRLab3/run.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
-python main.py -d cuda:0 -n resnet -t
-python main.py -d cuda:0 -n vgg -t
+python main.py -d cuda:0 -n resnet -t -l 0.16 -o sgd
+python main.py -d cuda:0 -n vgg -t -l 0.16 -o sgd
+
+python main.py -d cuda:0 -n resnet -t -l 0.12 -o sgd
+python main.py -d cuda:0 -n vgg -t -l 0.12 -o sgd
+
+python main.py -d cuda:0 -n resnet -t -l 0.08 -o sgd
+python main.py -d cuda:0 -n vgg -t -l 0.08 -o sgd
+
+python main.py -d cuda:0 -n resnet -t -l 0.08 -o adam
+python main.py -d cuda:0 -n vgg -t -l 0.08 -o adam
+
+python main.py -d cuda:0 -n resnet -t -l 0.05 -o adam
+python main.py -d cuda:0 -n vgg -t -l 0.001 -o adam
